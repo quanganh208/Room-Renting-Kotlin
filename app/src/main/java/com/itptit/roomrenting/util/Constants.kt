7edd2c23e7717ae6a8dd5889b7e7db6803663239
quanganh208 <nquanganh478@gmail.com>
2024-11-28 16:38:12 +0700

    change: base url

--- app/src/main/java/com/itptit/roomrenting/util/Constants.kt
+++ app/src/main/java/com/itptit/roomrenting/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.itptit.roomrenting.util
 
 object Constants {
-    const val BASE_URL = "https://room-renting-1d9554e345c4.herokuapp.com/"
+    const val BASE_URL = "https://room-renting-api-production.up.railway.app/"
     const val URL_PROVINCES = "https://provinces.open-api.vn/api/"
     const val USER_SETTINGS = "user_settings"
     const val APP_ENTRY = "app_entry"
